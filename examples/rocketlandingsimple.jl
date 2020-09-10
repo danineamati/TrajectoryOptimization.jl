635f33b8017dd58e915889e6bf01842f9a7e99b3
Daniel Neamati <danineamati@gmail.com>
2020-09-10 12:51:52 -0700

    Added Plotting Capabilities

--- examples/rocketlandingsimple.jl
+++ examples/rocketlandingsimple.jl
@@ -75,22 +75,43 @@ n, m = size(model)
 # m is the size of the control thrust ([Tx, Ty, Tz]])
 
 # Trajectory Discretization
-N = 151
-tf = 5.
+N = 251
+tf = 10.
 dt = tf/(N-1)
 
 # Initial and Final Conditions
-x0 = @SVector [1.0, 0.0, 20.0, -0.1, 0.0, -5.0] # Start at a 20 m altitude
+x0 = @SVector [1.0, 0.0, 20.0, -0.1, 0.0, -19.0] # Start at a 20 m altitude
 xf = @SVector zeros(n)  # Swing pendulum up and end at rest
 
 # LQR Objective Set Up
 Q = 1.0e-2*Diagonal(@SVector ones(n))
 Qf = 100.0*Diagonal(@SVector ones(n))
-R = 1.0*Diagonal(@SVector ones(m))
+R = 1.0e-1*Diagonal(@SVector ones(m))
 obj = LQRObjective(Q,R,Qf,xf,N)
 
+# Now we get into the other constraints
+# Create Empty ConstraintList
+conSet = ConstraintList(n,m,N)
+
+# Bounds
+ground_level = 0.0 # Crash Prevention Constraint
+umax = 11 * model.mass # Simplified Max Thrust Constraint
+theta = 20 # deg. Max Angle Constraint
+ulateral_max = umax * sind(theta)
+# bnd = BoundConstraint(n,m, x_min=[-Inf, -Inf, ground_level,
+#                                   -Inf, -Inf, -Inf])
+bnd = BoundConstraint(n,m, x_min=[-Inf, -Inf, ground_level,
+                                  -Inf, -Inf, -Inf],
+                           u_min=[-ulateral_max, -ulateral_max, 0   ],
+                           u_max=[ ulateral_max,  ulateral_max, umax])
+add_constraint!(conSet, bnd, 1:N-1)
+
+# Goal Constraint that the rocket must reach the landing site.
+goal = GoalConstraint(xf)
+add_constraint!(conSet, goal, N)
+
 # Package the objective and constraints into a "problem" type
-prob = Problem(model, obj, xf, tf, x0=x0)
+prob = Problem(model, obj, xf, tf, x0=x0, constraints=conSet)
 
 # Now, initialize the trajectory
 u0 = model.g # controls that would naminally hover
@@ -110,15 +131,45 @@ set_options!(altro, show_summary=true)
 solve!(altro)
 
 X = states(altro)
+U = controls(altro)
 
 # Now we want to plot the results
+println("Beginning Plotting")
+
 xs = [x[1] for x in X]
 zs = [x[3] for x in X]
 
-plt = plot(xs, zs, label = "Trajectory")
+uxs = [u[1] for u in U]
+uzs = [u[3] for u in U]
+
+titleText = "Rocket Soft-Landing Trajectory \n" *
+        "starting at an initial $(x0[6]) m/s plummet"
+filename = "RocketGroundThrustBound_BarelyFeasible3"
+
+plt_x = plot(xs, zs, label = "Trajectory")
 xlabel!("x")
 ylabel!("z (height)")
-display(plt)
+title!(titleText)
+display(plt_x)
+savefig(filename * "_Trajectory")
+
+plt_ux = plot(uxs, label = "Ux Control")
+hline!([-ulateral_max, ulateral_max], linecolor = :grey, linestyle = :dash,
+                label = "Max Lateral Thrust")
+# xlabel!("time (s)")
+ylabel!("control (N)")
+title!("Controls over Time")
+
+plt_uz = plot(uzs, label = "Uz Control")
+hline!([umax], linecolor = :grey, linestyle = :dash, label = "Max Thrust")
+xlabel!("time (s)")
+ylabel!("control (N)")
+
+plt_u = plot(plt_ux, plt_uz, layout = (2, 1))
+display(plt_u)
+savefig(filename * "_Controls")
+
+println("Beginning Animation")
 
 xmin = minimum(xs)
 xmax = maximum(xs)
@@ -126,12 +177,15 @@ xmax = maximum(xs)
 zmin = minimum(zs)
 zmax = maximum(zs)
 
-@gif for i in 1:N
-    plt = plot(xs[1:i], zs[1:i], label = "Trajectory")
+animSimple = @animate for i in 1:N
+    plt = plot(xs[1:i], zs[1:i], label = "Trajectory", legend = :topleft)
     xlabel!("x")
     ylabel!("z (height)")
     xlims!(xmin,xmax)
     ylims!(zmin,zmax)
+    title!(titleText)
 end
+
+gif(animSimple, filename * "_anim.gif")
 
 println("Complete!")
